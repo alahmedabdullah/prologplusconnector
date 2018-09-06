2e43f8e85cb575f440b758e7a7f7072388abb209
James Elligett <41028207+jelligett@users.noreply.github.com>
2018-09-06 16:00:37 +1000

    Add java to PATH

--- payload_prologplus/process_payload/run.sh
+++ payload_prologplus/process_payload/run.sh
@@ -6,6 +6,9 @@ OUTPUT_DIR=$2
 cd $INPUT_DIR
 prolog_exe=$(whereis gprolog 2>&1 | awk '/gprolog/ {print $2}')
 
+java_exe=$(whereis java 2>&1 | awk '/java/ {print $2}')
+java_path=$(dirname $java_exe)
+export PATH=$PATH:$java_path
 
 $prolog_exe $(cat gprolog_command_line.txt) &> runlog.txt
 
